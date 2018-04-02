fd4dfe948616c1e39bc8ae4a31e0a90313a012a4
tworden <tracy.worden@gmail.com>
2018-04-02 12:38:04 -0500

    Initial commit

---
+++ HylightSensor.py
@@ -0,0 +1,62 @@
+#!/usr/local/bin/python
+
+import RPi.GPIO as GPIO
+import time
+
+__author__ = 'Gus (Adapted from Adafruit)'
+__license__ = "GPL"
+__maintainer__ = "pimylifeup.com"
+
+GPIO.setmode(GPIO.BOARD)
+
+#define the pin that goes to the circuit
+pin_to_circuit = 7
+pin_to_led = 16
+
+def rc_time (pin_to_circuit):
+    count = 0
+  
+    #Output on the pin for 
+    GPIO.setup(pin_to_circuit, GPIO.OUT)
+    GPIO.output(pin_to_circuit, GPIO.LOW)
+    time.sleep(0.1)
+
+    #Change the pin back to input
+    GPIO.setup(pin_to_circuit, GPIO.IN)
+  
+    #Count until the pin goes high
+    while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+        count += 1
+
+    return count
+
+def ToggleLed (pin_to_led,toggle):
+
+    GPIO.setup(pin_to_led,GPIO.OUT)
+    if (toggle):
+      print("LED on")
+      GPIO.output(pin_to_led,GPIO.HIGH)
+      time.sleep(1)
+    else:
+      print("LED off")
+      GPIO.output(pin_to_led,GPIO.LOW)
+
+#Catch when script is interupted, cleanup correctly
+try:
+    # Main loop
+    print('Starting')
+    rcTime = 0
+    
+    while True:
+        rcTime=rc_time(pin_to_circuit)
+        print('Time = {}'.format(rcTime))
+        if rcTime > 2000:
+            ToggleLed(pin_to_led,True)
+        else:
+            ToggleLed(pin_to_led,False)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
